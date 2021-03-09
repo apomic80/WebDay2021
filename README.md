b31f0e7b0d70ae8ba91293b4686719e4d101f118
apomic80 <apomic80@hotmail.com>
2021-03-09 05:50:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Generazione dinamica della UI con Blazor WebAsse,by - WebDay 2021
+
+Codice della sessione Generazione dinamica della UI con Blazor WebAssembly tenuta al WebDay 2021.
+
+Slide: https://www.slideshare.net/apomic80/generazione-dinamica-della-ui-con-blazor-webassembly
---
+++ Shared/WeatherForecast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace webday2021.Shared
+{
+    public class WeatherForecast
+    {
+        [DisplayFormat(DataFormatString="{0:dd-MM-yyyy}")]
+        public DateTime Date { get; set; }
+
+        [Display(Name="Temp (C)")]
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        [Display(Name="Temp (F)")]
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
